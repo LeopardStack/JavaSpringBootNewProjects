0b4a5ecfa272b7434a17314b5ae892d4ea626888
leopard <lm2904083750@gmail.com>
2026-03-24 20:33:13 +0800

    Initial Spring Boot project

---
+++ src/main/java/com/scnu/springbootjdk17demo/SpringBootJdk17DemoApplication.java
@@ -0,0 +1,13 @@
+package com.scnu.springbootjdk17demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJdk17DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJdk17DemoApplication.class, args);
+    }
+
+}
